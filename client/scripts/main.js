7183884f42359b8bb6196f4b9e9594cbfbd8e1ce
Bartosz Galek <bartosz.galek@allegrogroup.com>
2016-02-27 13:34:30 +0100

    updated views

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import styles from '../styles/styles.less';
-
 import App from './components/app';
 
 ReactDOM.render(<App />, document.querySelector('#main'));
