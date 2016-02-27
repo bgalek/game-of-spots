f26cb63dd8dd351e5bcd43866929c2052c50da9c
Bartosz Galek <bartosz.galek@allegrogroup.com>
2016-02-27 15:35:17 +0100

    tabs fix

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from './components/App'
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap, can go away when react 1.0 release
+injectTapEventPlugin();
 
 ReactDOM.render(<App />, document.querySelector('#main'));
