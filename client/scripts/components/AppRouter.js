e4cbcfc4b31d7027844f90c17647fcefe36352de
Krzysztof Borkowski <krzysiek.borkowski@gmail.com>
2016-02-27 13:58:30 +0100

    router changes

--- client/scripts/components/AppRouter.js
+++ client/scripts/components/AppRouter.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { Router, Route, Link } from 'react-router'
+import { Router, Route, Link, browserHistory } from 'react-router'
 
-import App from 'App'
-import Chat from 'Chat'
-import Error404 from 'Error404'
+import App from './App'
+import Chat from './Chat'
+import Error404 from './Error404'
 
 export default React.createClass({
-    render((
+    render() {
       <Router history={browserHistory}>
         <Route path="/" component={App}>
           <Route path="/chats/:chatId" component={Chat}/>
           <Route path="*" component={Error404}/>
         </Route>
       </Router>
-    ), document.body)
+    }
 });
