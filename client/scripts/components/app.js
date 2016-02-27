d7a57620da8a01d7a4d75af460117df9bc1be053
Bartosz Galek <bartosz.galek@allegrogroup.com>
2016-02-27 13:01:29 +0100

    test version 1

--- client/scripts/components/app.js
+++ client/scripts/components/app.js
@@ -1,6 +1,10 @@
-import React from 'react';
-
-import repositories from '../api/chats';
+import React from "react";
+import AppBar from "material-ui/lib/app-bar";
+import Avatar from "material-ui/lib/avatar";
+import List from "material-ui/lib/lists/list";
+import ListItem from "material-ui/lib/lists/list-item";
+import Divider from "material-ui/lib/divider";
+import CommunicationChatBubble from "material-ui/lib/svg-icons/communication/chat";
 
 export default React.createClass({
     displayName: 'App',
@@ -8,7 +12,45 @@ export default React.createClass({
     render() {
         return (
             <div className="app">
-                <h1>App</h1>
+                <AppBar title="Game of Spots"/>
+                <List subheader="Available spots">
+                    <ListItem
+                        primaryText="Brendan Lim"
+                        leftAvatar={<Avatar src="http://www.material-ui.com/images/ok-128.jpg" />}
+                        rightIcon={<CommunicationChatBubble />}
+                    />
+                    <ListItem
+                        primaryText="Eric Hoffman"
+                        leftAvatar={<Avatar src="http://www.material-ui.com/images/kolage-128.jpg" />}
+                        rightIcon={<CommunicationChatBubble />}
+                    />
+                    <ListItem
+                        primaryText="Grace Ng"
+                        leftAvatar={<Avatar src="http://www.material-ui.com/images/uxceo-128.jpg" />}
+                        rightIcon={<CommunicationChatBubble />}
+                    />
+                    <ListItem
+                        primaryText="Kerem Suer"
+                        leftAvatar={<Avatar src="http://www.material-ui.com/images/kerem-128.jpg" />}
+                        rightIcon={<CommunicationChatBubble />}
+                    />
+                    <ListItem
+                        primaryText="Raquel Parrado"
+                        leftAvatar={<Avatar src="http://www.material-ui.com/images/raquelromanp-128.jpg" />}
+                        rightIcon={<CommunicationChatBubble />}
+                    />
+                </List>
+                <Divider />
+                <List subheader="Previous chats">
+                    <ListItem
+                        primaryText="Chelsea Otakan"
+                        leftAvatar={<Avatar src="http://www.material-ui.com/images/chexee-128.jpg" />}
+                    />
+                    <ListItem
+                        primaryText="James Anderson"
+                        leftAvatar={<Avatar src="http://www.material-ui.com/images/jsa-128.jpg" />}
+                    />
+                </List>
             </div>
         );
     }
